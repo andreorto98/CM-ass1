759c7b4648d494ea3ee8d4e0dfa70fe38198e0bd
andreorto98 <andrea.ortone98@gmail.com>
2020-10-05 21:32:20 +0200

    inizio del progetto

---
+++ ass1.py
@@ -0,0 +1,45 @@
+import argparse
+import logging
+import time
+import string
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def process(file_path):
+    start_time = time.time()
+
+    logging.info('Reading input file %s ...', file_path) # virgola, anzichè % file_path. il punto è che logging è fatto in modo da formattare la stringa solo se deve stampare la particolare stringa-->dipende dal level di logging
+    with open(file_path) as input_file:
+        # with permette di aprire il file in modo più bello: quando esco dal with il file viene chiuso automaticamente!!
+        text = input_file.read()
+    num_chars = len(text)
+    logging.info('Done, %d characters found', num_chars)
+
+    char_dict = {ch : 0 for ch in string.ascii_lowercase}
+    for ch in text:
+        try:
+            char_dict[ch.lower()] = char_dict[ch.lower()] + 1
+        except KeyError:
+            pass
+    # KeyError viene sollevato quando tento di accedere a un dizionario con una chiave che non esiste
+
+    # Il libro vero inizia con I went down
+
+    num_letters = sum(char_dict.values())
+    logging.debug(char_dict)
+    logging.info('Done, %d valid characters found', num_letters)
+    for ch, num in char_dict.items():
+        print(f'{ch} -> {num/num_letters:.3%}')
+
+    elapsed_time = time.time() - start_time
+    logging.info('Done in %.4f seconds', elapsed_time)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type = str, help='Path to the input file')
+    #parser.add_argument('hist', type = int, help='set 1 to create histogram of letter-occurrences, set 0 to not create the histogram')
+    args = parser.parse_args()
+    process(args.infile
